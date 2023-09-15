09e650c831696bd1ba5a01e875d1ccdb4c7f9c65
e.vladimirova24 <e.vladimirova24@LAB108PC02>
2023-09-15 09:38:28 +0300

    Add project files.

---
+++ ConditionalStatements3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("#2");
+Console.WriteLine("Choose a number?");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 ==0 || n==0)
+{
+    Console.WriteLine(n + " is and even integer");
+}
+else
+{
+    Console.WriteLine(n + " is an odd integer");
+}
